ed97283460d8e66b86e0cfc80c21f2f8df76f43c
John <coldlombax@gmail.com>
2021-07-07 22:58:02 +0100

    Topbar added to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import BottomNavbar from './components/BottomNavbar';
+import TopBar from './components/TopBar';
 
 export default function App() {
   return (
     <div>
+      <TopBar />
       <BottomNavbar />
     </div>
   );
