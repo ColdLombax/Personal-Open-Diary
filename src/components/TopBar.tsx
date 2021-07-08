26883accab6f7601cc4fef1e5a043a5b7bba26cf
John <coldlombax@gmail.com>
2021-07-08 11:53:13 +0100

    Removed topbar.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,29 +1,7 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-import { AppBar, Button } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles({
-  bar: {
-    display: 'flex',
-    flexDirection: 'row',
-    padding: '5px',
-  },
-  title: {
-    marginRight: 'auto',
-  },
-});
-
 export default function TopBar() {
-  const classes = useStyles();
   return (
-    <AppBar position="static" className={classes.bar}>
-      <Typography variant="h5" component="h1" className={classes.title}>
-        Open Diary
-      </Typography>
-      <Button color="inherit">Login</Button>
-    </AppBar>
+    <div />
   );
 }
